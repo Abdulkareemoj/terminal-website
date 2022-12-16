ca1422b51194010b08196fb7972e3ffa22bbbc50
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2022-12-16 10:06:48 +0000

    add some code

--- app/js/commands.js
+++ app/js/commands.js
@@ -1,22 +1,79 @@
-help = [
-    'whois',
-    'why',
-    'projects',
-    'contact',
-    'secret'
-]
-
-var linkedin;
-var github;
+var youtube = "https://www.youtube.com/";
+var twitter = "https://www.twitter.com/";
+// var password = "";
+var linkedin = "https://www.linkedin.com/in/";
+var snapchat = "https://www.snapchat.com/";
+var github = "https://github.com/abdulkareemoj/";
+var email = 'mailto:abdulkareemojerinde@gmil.com';
 
 whois = [
-    "<br>",
-    "Hi there", 
-    "<br>"
+  "<br>",
+  "Hey, I'm Forrest!ðŸ‘‹",
+  "I'm a software developer and content creator, who builds engaging websites like this one",
+  "and makes YouTube videos about computer science & software engineering.",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a software engineer building enterprise web applications for Fortune 500 companies.",
+  "While doing all of that, I documentned my coding journey on YouTube - trying to enlighten",
+  "the next generation of developers and help them navigate the crazy world that is software", "development & computer science.",
+  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
+  "I needed to make the jump from software engineering to full time content creator, and it's",
+  "the best decision I ever made.",
+  "Now, I make videos about creating cool shit like this terminal website, and hosting my",
+  "podcast 'Decoded w/ Forrest Knight.' What most people don't know, and will only know",
+  "because they're reading this right now, is that I also run a creative & media agency.",
+  "We partner with clients to drive their business outcomes using modern marketing strategies.",
+  "<br>"
 ];
 
-why = [
-    "<br>",
-    "I thought this would be fun",
-    "<br>"
-]
+whoami = [
+  "<br>",
+  "The paradox of â€œWho am I?â€ is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/forrestpknight' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/forrestpknight' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/forrestpknight' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
+  "<br>"
+];
+
+
+projects = [
+  "<br>",
+  "Uhhh...",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Forrest?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">WiZtEk, Not A Corporation. All knights reserved.</span>',
+//   "         ___   ____",
+//   "       /' --;^/ ,-_\\     \\ | /       ",
+//   "      / / --o\\ o-\\ \\\\   --(_)--   ",
+//   "     /-/-/|o|-|\\-\\\\|\\\\   / | \\   ______                          __     __ __       _       __    __   ",
+//   "      '`  ` |-|   `` '          / ____/___  _____________  _____/ /_   / //_/____  (_)___ _/ /_  / /_ ",
+//   "            |-|                / /_  / __ \\/ ___/ ___/ _ \\/ ___/ __/  / ,<  / __ \\/ / __ `/ __ \\/ __/ ",
+//   "            |-|O              / __/ / /_/ / /  / /  /  __(__  ) /_   / /| |/ / / / / /_/ / / / / /_  ",
+//   "            |-(\\,__          /_/    \\____/_/  /_/   \\___/____/\\__/  /_/ |_/_/ /_/_/\\__, /_/ /_/\\__/ ",
+//   "         ...|-|\\--,\\_....                                                         /____/    Â© 2022",
+//   "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.   ",
+//   "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+//   '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
